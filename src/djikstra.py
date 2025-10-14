91ed190e88ecbf1a6ba061b27c0de8a7348e42a2
Sam Martin-Brown <sam.martin@withsecure.com>
2025-10-14 13:01:27 +0100

    WIP: some leftover bits

--- src/djikstra.py
+++ src/djikstra.py
@@ -1,9 +1,14 @@
 from __future__ import annotations
 
+import argparse
+import logging
 from dataclasses import dataclass
+from heapq import heappop, heappush
 import typing as t
 
 MapT = list[list[int]]
+
+LOG = logging.getLogger(__name__)
 
 
 @dataclass
@@ -13,17 +18,27 @@ class Pos:
     dist: int = -1
     previous: Pos | None = None
 
+    def __lt__(self, other: object) -> bool:
+        if not isinstance(other, Pos):
+            raise NotImplementedError
+
+        return self.dist < other.dist
+
 
 MAP: MapT = [
-    [1, 0, 0],
-    [0, 1, 0],
-    # [0, 1, 0],
-    [0, 0, 0],
-    [0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0],
 ]
 
-START = Pos(1, 0)
-END = Pos(0, 1)
+START = Pos(0, 0, 0)
+END = Pos(5, 8)
 
 
 def can_move(target: Pos, map: MapT) -> bool:
@@ -42,9 +57,9 @@ def can_move(target: Pos, map: MapT) -> bool:
 def find_neighbours(cur_pos: Pos, map: MapT) -> t.Iterator[Pos]:
     cardinals = (
         # North
-        Pos(cur_pos.x , cur_pos.y - 1, cur_pos.dist + 1, cur_pos),
+        Pos(cur_pos.x, cur_pos.y - 1, cur_pos.dist + 1, cur_pos),
         # South
-        Pos(cur_pos.x , cur_pos.y + 1, cur_pos.dist + 1, cur_pos),
+        Pos(cur_pos.x, cur_pos.y + 1, cur_pos.dist + 1, cur_pos),
         # East
         Pos(cur_pos.x - 1, cur_pos.y, cur_pos.dist + 1, cur_pos),
         # West
@@ -56,50 +71,73 @@ def find_neighbours(cur_pos: Pos, map: MapT) -> t.Iterator[Pos]:
         yield c
 
 
+def print_map(map: MapT) -> None:
+    total_traversible = sum(
+        1 for y in range(len(map)) for x in range(len(map[y])) if map[y][x] == 0
+    )
+    LOG.debug(f"Traversible cells: {total_traversible}")
+
+    print(f"   x: {', '.join(str(i) for i in range(len(map[0])))}")
+    for x in range(len(MAP)):
+        print(f"y: {x} {MAP[x]}")
+
+
 def main() -> None:
     iter = 0
 
     to_visit = [START]
     visited: dict[tuple[int, int], Pos] = {}
-    # visited = []
-    print("   x: 0, 1, 2")
-    for x in range(len(MAP)):
-        print(f"y: {x} {MAP[x]}")
+    print_map(MAP)
 
     while to_visit:
+        iter += 1
         if iter > 1_000:
             raise Exception("something fucked up...")
 
-        next = to_visit.pop()
+        next = heappop(to_visit)
         if next.x == END.x and next.y == END.y:
             print(f"Destination reached: {next}")
+            visited[next.x, next.y] = next
             break
 
-        print(f"visiting {next}")
+        LOG.debug(f"visiting {next}")
         for neighbour in find_neighbours(next, MAP):
             neighbour_pos = neighbour.x, neighbour.y
             if neighbour_pos not in visited:
-                to_visit.append(neighbour)
+                heappush(to_visit, neighbour)
             # Don't revisit neighbours if we've been there before for less distance.
             elif visited[neighbour_pos].dist > neighbour.dist:
-                    to_visit.append(neighbour)
+                heappush(to_visit, neighbour)
             else:
-                print(f"Skipping neighbour: {neighbour_pos} for distance {neighbour.dist}, already visited for dist: {visited[neighbour_pos].dist}")
-
+                LOG.debug(
+                    f"Skipping neighbour: {neighbour_pos} for distance {neighbour.dist}, already visited for dist: {visited[neighbour_pos].dist}"
+                )
 
         pos = (next.x, next.y)
         if pos not in visited:
-            print(f"Storing entry for new pos {pos}")
+            LOG.debug(f"Storing entry for new pos {pos}")
             visited[pos] = next
             continue
         if next.dist < visited[pos].dist:
-            print(f"Found new path to {pos} for dist {next.dist} (prev. {visited[pos].dist})")
+            LOG.info(
+                f"Found new path to {pos} for dist {next.dist} (prev. {visited[pos].dist})"
+            )
             visited[pos] = next
             continue
         else:
-            print(f"not updating {pos}, already found a shorter route.")
+            LOG.debug(f"not updating {pos}, already found a shorter route.")
 
+    end_pos = (END.x, END.y)
+    if end_pos not in visited:
+        print("Failed to resolve path!")
+        print(f"total iter count: {iter}")
+        return
+
+    print_map(MAP)
+
+    next = visited[end_pos]
     path = [next]
+    print(path)
     while True:
         if next.previous:
             next = next.previous
@@ -112,6 +150,18 @@ def main() -> None:
     for i in path:
         print(f"{i.x, i.y}")
 
+    print(f"path length: {len(path)}")
+    print(f"total iter count: {iter}")
+
 
 if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument(
+        "--log-level",
+        default="INFO",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+        help="Set the logging level"
+    )
+    parsed_args = args.parse_args()
+    logging.basicConfig(level=getattr(logging, parsed_args.log_level))
     main()
--- src/main.c
+++ src/main.c
@@ -1,13 +1,21 @@
+#include "memory.h"
 #include "raylib.h"
 #include "stdio.h"
+#include <math.h>
 
 #define X_SIZE 3
 #define Y_SIZE 4
 #define INVALID_MOVE_INDICATOR_TIME 0.3
 
+enum terrain {
+  GROUND = 0,
+  WALL = 1,
+};
+
 typedef struct {
   int x;
   int y;
+  int passable;
 } Pos;
 
 typedef struct {
@@ -16,7 +24,15 @@ typedef struct {
   float false_move;
 } CharState;
 
+typedef struct {
+  Pos pos;
+  float dist;
+  int parent_idx;
+} Distance;
+
 typedef int MAP[Y_SIZE][X_SIZE];
+
+#define MAX_PATH Y_SIZE *X_SIZE
 
 int can_move(Pos target_pos, const MAP *map) {
   if (target_pos.y >= Y_SIZE) {
@@ -37,6 +53,152 @@ int can_move(Pos target_pos, const MAP *map) {
   }
 
   return 1;
+}
+
+// A python generator would be _perfect_ here...
+int find_neighbours(Pos cur_pos, const MAP *map, Pos *neighbours) {
+  int count = 0;
+
+  // Diagonals and origin should not be returned.
+  // This leaves essentially cardinal directions only.
+  Pos cardinals[4] = {
+      // North
+      {cur_pos.x, cur_pos.y - 1},
+      // South
+      {cur_pos.x, cur_pos.y + 1},
+      // East
+      {cur_pos.x - 1, cur_pos.y},
+      // West
+      {cur_pos.x + 1, cur_pos.y},
+  };
+
+  for (int idx = 0; idx < 4; idx++) {
+    if (can_move(cardinals[idx], map)) {
+      neighbours[count] = cardinals[idx];
+      count++;
+    }
+  }
+  return count;
+}
+
+int calculate_path(Pos *path, int max_path_size, Pos player_pos, Pos dest,
+                   int (*map)[Y_SIZE][X_SIZE]) {
+  printf("searching for path from (%d, %d) to (%d, %d)\n", player_pos.x,
+         player_pos.y, dest.x, dest.y);
+  // Djikstra path finding.
+  Distance unvisited[Y_SIZE * X_SIZE];
+  Distance visited[Y_SIZE * X_SIZE];
+  // Pos visited[Y_SIZE * X_SIZE];
+  // int visit_count = 0;
+  int found = 0;
+  int total_unvisited = 1;
+  int total_visited = 0;
+  int count = 0;
+
+  // set start pos
+  Pos cur_pos = {player_pos.x, player_pos.y};
+  unvisited[0] = (Distance){cur_pos, 0, -1};
+
+  while (found == 0) {
+    count++;
+    if (count > pow(Y_SIZE * X_SIZE, 2)) {
+      printf("it was then he realised, he fucked up...\n");
+      break;
+    };
+    // visit the shorted-dist node of 'unvisited'
+    // find neighbours of current node
+    // add viable neighbours to unvited
+    // record path if shorter than existing path to current node in
+    // 'visisted'
+    int closest_dist = 0;
+    int closest_node = 0;
+    for (int idx = 0; idx < total_unvisited; idx++) {
+      Distance *node = &unvisited[idx];
+      if (node->dist < closest_dist) {
+        closest_node = idx;
+        closest_dist = node->dist;
+      }
+    }
+
+    if (total_unvisited <= 0) {
+      printf("Nothing more to explore...\n");
+      return -1;
+    } else {
+      printf("total_unvisited: %d\n", total_unvisited);
+    }
+    Distance cur_node = unvisited[closest_node];
+
+    // 'pop' cur_node out of unvisited... (feck)
+    unvisited[closest_node] = unvisited[total_unvisited - 1];
+    total_unvisited--;
+
+    // Find neighbours of current node...
+    Pos neighbours[4];
+    int num_neighbours = find_neighbours(cur_pos, map, neighbours);
+
+    // Add each neighbour to visit
+    for (int idx = 0; idx < num_neighbours; idx++) {
+      Pos *neighbour = &neighbours[idx];
+      Distance new_target = {
+          {neighbour->y, neighbour->x}, 1 + cur_node.dist, total_visited};
+      // Check if neighbour is already due to be visited and overwrite if
+      // we have a shorter path...
+      int found_new = 0;
+      for (int idx = 0; idx < total_unvisited; idx++) {
+        if (memcmp(&unvisited[idx].pos, &new_target.pos, sizeof(Pos)) == 0) {
+          found_new = 1;
+          if (new_target.dist < unvisited[idx].dist) {
+            printf("found shorter neighbour: (%d, %d): %f, parent_id: %d\n",
+                   new_target.pos.x, new_target.pos.y, new_target.dist,
+                   new_target.parent_idx);
+            unvisited[idx] = new_target;
+          } else {
+            printf("found longer neighbour: (%d, %d): %f, parent_id: %d\n",
+                   new_target.pos.x, new_target.pos.y, new_target.dist,
+                   new_target.parent_idx);
+            // Existing node path is already shorter, bail.
+            break;
+          }
+        }
+        if (found_new == 0) {
+          printf("found new neighbour: (%d, %d): %f, parent_id: %d\n",
+                 new_target.pos.x, new_target.pos.y, new_target.dist,
+                 new_target.parent_idx);
+          unvisited[total_unvisited] = new_target;
+          total_unvisited++;
+        }
+      }
+    }
+
+    // mark cur_node as visited...
+    visited[total_visited] = cur_node;
+    total_visited++;
+    if (memcmp(&cur_node.pos, &dest, sizeof(Pos)) == 0) {
+      found = 1;
+    }
+  }
+
+  // Figure out wtf we did...
+  printf("total_visited: %d\n", total_visited);
+  Distance backwards_path[Y_SIZE * X_SIZE];
+  backwards_path[0] = visited[total_visited - 1];
+  int back_p_idx = 0;
+  printf("total_unvisited: %d, total_visited: %d, back_p_idx: %d\n",
+         total_unvisited, total_visited, back_p_idx);
+  while (true) { // TODO: Don't leave this wank in here...
+    Distance prev = backwards_path[back_p_idx];
+    if (prev.parent_idx > -1) {
+      back_p_idx++;
+      backwards_path[back_p_idx] = visited[prev.parent_idx];
+    } else {
+      break;
+    }
+  }
+
+  for (int idx = back_p_idx; idx > -1; idx--) {
+    path[total_visited - idx] = backwards_path[idx].pos;
+  }
+  return total_visited;
 }
 
 int main() {
@@ -64,9 +226,14 @@ int main() {
   const int char_width = rect_width * 0.8;
   const int char_height = rect_height * 0.8;
 
+  int moved = 1;
   CharState character = {1, 0, 0.0};
   const int char_x_offset = (rect_width - char_width) / 2;
   const int char_y_offset = (rect_height - char_height) / 2;
+
+  const Pos destination = {0, 1};
+
+  Pos path[MAX_PATH];
 
   while (!WindowShouldClose()) {
     BeginDrawing();
@@ -80,14 +247,11 @@ int main() {
     }
 
     // Draw character
-
     Color c_colour = YELLOW;
     if (character.false_move > 0) {
       c_colour = RED;
       const float since_last_frame = GetFrameTime();
-      character.false_move = character.false_move < since_last_frame
-                                 ? 0
-                                 : character.false_move - since_last_frame;
+      character.false_move = character.false_move - since_last_frame;
     }
 
     DrawRectangle((character.x * rect_width) + char_x_offset,
@@ -98,6 +262,7 @@ int main() {
       const Pos new_pos = {character.x, character.y + 1};
       if (can_move(new_pos, &map)) {
         character.y += 1;
+        moved = 1;
       } else {
         character.false_move = INVALID_MOVE_INDICATOR_TIME;
       }
@@ -106,6 +271,7 @@ int main() {
       const Pos new_pos = {character.x, character.y - 1};
       if (can_move(new_pos, &map)) {
         character.y -= 1;
+        moved = 1;
       } else {
         character.false_move = INVALID_MOVE_INDICATOR_TIME;
       }
@@ -114,6 +280,7 @@ int main() {
       const Pos new_pos = {character.x - 1, character.y};
       if (can_move(new_pos, &map)) {
         character.x -= 1;
+        moved = 1;
       } else {
         character.false_move = INVALID_MOVE_INDICATOR_TIME;
       }
@@ -122,12 +289,27 @@ int main() {
       const Pos new_pos = {character.x + 1, character.y};
       if (can_move(new_pos, &map)) {
         character.x += 1;
+        moved = 1;
       } else {
         character.false_move = INVALID_MOVE_INDICATOR_TIME;
       }
     }
 
+    // Render path to dest
+    if (moved == 1) {
+      int path_len =
+          calculate_path(&path[0], MAX_PATH, (Pos){character.x, character.y},
+                         destination, &map);
+
+      printf("path:\n");
+      for (int idx = 0; idx < path_len; idx++) {
+        printf("%d: (%d, %d)\n", idx, path[idx].x, path[idx].y);
+      }
+      // TODO: render path array here...
+    }
+
     EndDrawing();
+    moved = 0;
   }
 
   CloseWindow();
